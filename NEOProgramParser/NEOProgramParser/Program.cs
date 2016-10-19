e546bb2a645786bee2910b089d346b5a70acd739
Michael Fuglsang <Michael Fuglsang>
2016-10-19 14:12:56 +0200

    Pretty printing of program name and bytes

---
+++ NEOProgramParser/NEOProgramParser/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEOProgramParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            args = new string[] { "D:\\GitRepos\\NEOProgramParser\\NEOProgramParser\\NEOProgramParser\\bin\\Debug\\NB241017.GBC" };
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No path found");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Path: " + args[0] + "\n");
+                new ProgramParser(args[0]);
+            }
+        }
+    }
+}
---
+++ NEOProgramParser/NEOProgramParser/ProgramParser.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace NEOProgramParser
+{
+    class ProgramParser
+    {
+        List<byte> loadedBytes;
+
+        byte[] programName = new byte[8];
+
+        public ProgramParser(string path)
+        {
+            loadedBytes = new List<byte>(File.ReadAllBytes(path));
+
+            GetProgramName();
+
+            Console.WriteLine("Program Name: " + PrettyPrintProgramName());
+            Console.Write(this.PrettyPrintLoadedBytes());
+
+            Console.ReadKey();
+        }
+
+        void GetProgramName()
+        {
+            for (int i = 0; i < programName.Length; i++)
+            {
+                programName[i] = loadedBytes[1 + i];
+            }
+        }
+
+        string PrettyPrintProgramName()
+        {
+            string s = "";
+
+            foreach (byte b in programName)
+            {
+                s += Convert.ToChar(b);
+            }
+
+            return s;
+        }
+
+        string PrettyPrintLoadedBytes()
+        {
+            string s = "";
+
+            for (int i = 0; i < loadedBytes.Count; i++)
+            {
+                s += loadedBytes[i].ToString("x2");
+
+                if ((i + 1) % 2 == 0)
+                {
+                    s += " ";
+                }
+
+                if ((i + 1) % 16 == 0 && i > 0)
+                {
+                    s += "\n";
+                }
+            }
+
+            return s;
+        }
+    }
+}
